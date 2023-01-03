709c100eff343c76a88dc325c7a6fa8b3f2cd98d
sherlin2003 <swethasam2014@gmail.com>
2023-01-03 21:40:21 +0530

    code

---
+++ src/editor/About.java
@@ -0,0 +1,14 @@
+package editor;
+import javax.swing.JOptionPane; 
+
+public class About {
+
+    public About() {
+
+        JOptionPane.showMessageDialog(null, 
+        "Package Done by 20pw10 - Dhivya Lakshmi and 20pw35 - Swetha Muralidharan ", 
+        "About", JOptionPane.INFORMATION_MESSAGE);
+        
+    }
+
+}
